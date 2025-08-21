fbfaedef8d3d878e47f62fc2ed0f5a677d9bc3b7
Jack Fallas <jfallas-2021655@kinal.edu.gt>
2025-08-21 16:10:16 -0600

    Le metimos IA

--- src/main/java/org/jrae/holakinal/HolaController.java
+++ src/main/java/org/jrae/holakinal/HolaController.java
@@ -1,17 +1,25 @@
 package org.jrae.holakinal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Scanner;
 
 @RestController
 public class HolaController {
+    @Autowired
+    private final HolaServiceAI serviceAI;
+
+    public HolaController(HolaServiceAI serviceAI) {
+        this.serviceAI = serviceAI;
+    }
+
     @GetMapping("/")
     public String holaKinal(){
-        Scanner scanner = new Scanner(System.in);
-        int numero = 0;
-        System.out.print("Ingrese un numero: ");
-        numero = scanner.nextInt();
-        return "Hola Kinal, estoy en SB " + numero;
+//        Scanner scanner = new Scanner(System.in);
+//        int numero = 0;
+//        System.out.print("Ingrese un numero: ");
+//        numero = scanner.nextInt();
+        return "Hola Kinal, estoy en SB "/* + numero*/;
     }
 }
---
+++ src/main/java/org/jrae/holakinal/HolaServiceAI.java
@@ -0,0 +1,14 @@
+package org.jrae.holakinal;
+
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.spring.AiService;
+
+@AiService
+public interface HolaServiceAI {
+    @UserMessage("""
+            Genera un saludo de bienvenida a mi plataforma Pelicula en Linea jrae
+            usa al menos 120 caracteres y hazlo como un Chapin.
+            """)
+    String generarSaludo();
+
+}
